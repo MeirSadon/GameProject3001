ad6fc293638cb047d2b8bffa614ab588a68a1613
MeirSadon <45739995+MeirSadon@users.noreply.github.com>
2019-02-01 13:09:37 +0200

    Add files via upload

---
+++ GameProject 3001/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_3001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g1 = new Game
+            {
+                _name = "Monopol",
+                _numberOfPlayers = 8,
+                _originCountry = "Russian",
+                _rating = 4.1f
+            };
+
+            Game g2 = new Game("Football");
+            g2._numberOfPlayers = 6;
+            g2._originCountry = "Arab";
+            g2._rating = 0;
+
+            Game g3 = new Game("Chess", "Israel");
+            g3._numberOfPlayers = 4;
+            g3._rating = 10;
+
+            Game g4 = new Game("Computer", 3, 4.1f, "USA");
+            PrintGame(g4);
+
+        }
+        static void PrintGame(Game g)
+        {
+            int number;
+            Console.WriteLine(g);
+        }
+
+    }
+}
